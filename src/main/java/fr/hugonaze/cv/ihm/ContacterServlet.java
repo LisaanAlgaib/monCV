69be8a4c452a79cc2587761fe94195ab317cac1e
hnaze2022 <hnaze2022@154SE12-4L1J4V3.ad.campus-eni.fr>
2023-03-28 13:32:27 +0200

    1

--- src/main/java/fr/hugonaze/cv/ihm/ContacterServlet.java
+++ src/main/java/fr/hugonaze/cv/ihm/ContacterServlet.java
@@ -1,6 +1,7 @@
 package fr.hugonaze.cv.ihm;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 @WebServlet("/contacter")
 public class ContacterServlet extends HttpServlet {
@@ -19,10 +31,48 @@ public class ContacterServlet extends HttpServlet {
 		req.getRequestDispatcher("WEB-INF/pages/meContacter.jsp").forward(req, resp);
 		return;
 	}
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
 	
-	}
+		 @Override
+		 public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		   Properties props = new Properties();
+		   String email_mine = "hugo.naze.cv@gmail.com";
+		   String email_expediteur = req.getParameter("email");
+		   String sujet_email = req.getParameter("sujet");
+		   String corps_email = req.getParameter("corps_email");
+		   props.setProperty("mail.smtp.auth", "true");
+		   props.put("mail.transport.protocol", "smtp");
+		   props.put("mail.smtp.ssl.enable", false);
+		   props.put("mail.smtp.socketFactory.port", "465");
+	       props.put("mail.smtp.socketFactory.class", 
+	            "javax.net.ssl.SSLSocketFactory");
+		   props.put("mail.smtp.host", "smtp.gmail.com");
+		   props.put("mail.smtp.starttls.enable", false); 
+		   props.put("mail.smtp.port", 587); //TLS = 587  SSL = 465
+		   props.put("mail.from", email_mine);
+		   Session session = Session.getDefaultInstance(props, new Authenticator() {
+			   protected PasswordAuthentication getPasswordAuthentification() {
+				   return new PasswordAuthentication(email_mine, "efasqdxglcbieovf");
+			   }
+		   });
+
+		  try {
+		     Message msg = new MimeMessage(session);
+		     msg.setFrom(new InternetAddress(email_mine));
+		     msg.addRecipient(Message.RecipientType.TO,
+		                      new InternetAddress(email_mine));
+		     msg.setSubject(sujet_email);
+		     msg.setText(corps_email + " envoyé par : " + email_expediteur);
+		     //Transport.send(msg);
+		     System.out.println("Conception réussie");
+		     Transport transport = session.getTransport("smtp");
+			 transport.connect("smtp.gmail.com" , 587, email_mine, "efasqdxglcbieovf");
+			 transport.send(msg);
+			 System.out.println("message envoyé");
+		   } catch (AddressException e) {
+		     e.printStackTrace();
+		   } catch (MessagingException e) {
+		     e.printStackTrace();
+		   }		  
+	  }
 }
--- src/main/webapp/WEB-INF/pages/meContacter.jsp
+++ src/main/webapp/WEB-INF/pages/meContacter.jsp
@@ -11,5 +11,14 @@
 <title>Contact</title>
 </head>
 <body>
+<form action="<%= request.getContextPath()%>/contacter" method="post">
+	<label for="email">Email* : </label>
+	<input type="text" name="email" id="email" placeholder="example@mail.com" required min="5" max="40"><br>
+	<label for="sujet">Sujet* : </label>
+	<input type="text" name="sujet" id="sujet" max="60" placeholder="Objet" required><br>
+	<label for="corps_email">Message* : </label>
+	<textarea id="corps_email" name="corps_email" placeholder="Votre message" rows="5" cols="33" maxlength="300" required></textarea><br>
+	<input type="submit" value="Envoyer">
+</form>
 <%@ include file="/WEB-INF/parts/header.jsp" %>
 <%@ include file="/WEB-INF/parts/footer.jsp" %>
